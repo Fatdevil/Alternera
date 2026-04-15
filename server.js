baef39e94da73c448b290d686be8931c93247988
Stellan <stellanjonsson1976@gmail.com>
2026-04-16 01:21:52 +0200

    fix: resolve invalid token in server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(\`Server is running on port \${PORT}\`);
+  console.log('Server is running on port ' + PORT);
 });
